cc3007a8cd567b28f4b13d282b18417f4a80d901
Maksim Kostromin <daggerok@gmail.com>
2017-09-25 22:50:41 +0300

    Initial commit

---
+++ gh-pages/predeploy.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export REPLACEMENT=css-grid-layout-example
+
+# add base tag:
+# <head> -> <head><base href="/$REPLACEMENT/"/>
+
+if ! [ -z "$1" ]; then
+  sed -i -e "s/\(src=\"\/\)/src=\"\/$REPLACEMENT\//g" $1
+  sed -i -e "s/\(href=\"\/\)/href=\"\/$REPLACEMENT\//g" $1
+  sed -i -e "s/\(<head>\)/<head><base href=\"\/$REPLACEMENT\/\"\/>/g" $1
+fi
